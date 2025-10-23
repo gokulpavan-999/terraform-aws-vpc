f27feb2b8feaa67b6f56183ff88e867efa207129
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-23 17:42:38 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -43,4 +43,21 @@ resource "aws_subnet" "public" {
   )
 }
 
+# Public subnets 
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    var.public_subnet_tags,
+    local.common_tags,
+    {
+      Name = "${local.common_name_suffix}-public-${local.az_names[count.index]}" # roboshop-dev-public-us-east-1a
+    }
+  )
+}
+
 
