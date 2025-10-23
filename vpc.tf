6eac1ca73f2b479d1a8b5cff183e1f21e1f13291
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-23 15:23:53 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,1 +1,14 @@
+#VPC
+resource "aws_vpc" "main" {
+  cdir_block           = var.vpc_cdir
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
 
+  tags = merge(
+    var.vpc_tags,
+    local.common_tags,
+    {
+      Name = local.common_name_suffix
+    }
+  )
+}
