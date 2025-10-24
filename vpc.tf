f41df8355bfff84ff891420fcacd0abcad3ac7e6
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-24 11:55:58 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -113,3 +113,10 @@ resource "aws_route_table" "database" {
     }
   )
 }
+
+# Public Route 
+resource "aws_route" "public" {
+  route_table_id = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.main.id
+}
