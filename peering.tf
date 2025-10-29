a6bb0c6438f470e53ad4804572b0a2670f9e6185
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-29 14:21:47 +0530

    Update peering.tf

--- peering.tf
+++ peering.tf
@@ -1,6 +1,7 @@
 resource "aws_vpc_peering_connection" "default" {
   count = var.is_peering_required ? 1 : 0
   peer_vpc_id = data.aws_vpc.default.id # acceptor 
+  vpc_id = aws_vpc.main.id
 
   accepter {
     allow_remote_vpc_dns_resolution = true
