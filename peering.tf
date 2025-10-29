addeef8e3eb2af9c4503238bda2327e2f9c2e9f3
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-29 14:56:04 +0530

    Update peering.tf

--- peering.tf
+++ peering.tf
@@ -31,7 +31,7 @@ resource "aws_route" "public_peering" {
 
 resource "aws_route" "default_peering" {
   count = var.is_peering_required ? 1 : 0
-  route_table_id  = aws_route_table.main.id
+  route_table_id  = data.aws_route_table.main.id
   destination_cidr_block = var.vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
