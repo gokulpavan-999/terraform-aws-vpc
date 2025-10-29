a557fa4dd5240a5561175a968548c802c709049f
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-29 14:15:36 +0530

    Update peering.tf

--- peering.tf
+++ peering.tf
@@ -1,4 +1,22 @@
 resource "aws_vpc_peering_connection" "default" {
   count = var.is_peering_required ? 1 : 0
-  peer_vpc_id = data.aws_vpc.default.id # acceptor
+  peer_vpc_id = data.aws_vpc.default.id # acceptor 
+
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  } 
+
+  auto_accept = true
+
+  tags = merge(
+    var.vpc_tags,
+    local.common_tags,
+    {
+      Name = "${local.common_name_suffix}-default"
+    }
+  )
 }
